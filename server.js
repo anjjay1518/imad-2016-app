329e0fa23acba128a98ddbc57edac73fe1c20c1a
anjjay1518 <anjjay1518@gmail.com>
2016-09-23 13:42:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,15 +122,6 @@ app.get('/:articleName',function(req,res)
     res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two',function(req,res)
-{
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three',function(req,res)
-{
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
 app.get('/ui/style.css', function (req, res) { // if this particular url is rquested this one the below function will executed as a response.
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
