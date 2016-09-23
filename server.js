3386f5bff1d0cc9cccdfaa25997d0ff380eaf472
anjjay1518 <anjjay1518@gmail.com>
2016-09-23 13:26:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,64 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleOne = {
+    title:'Anjjay karthick | Webapp',
+    date:'Sep 23 2016',
+    heading:'Article-One',
+    content:`<p>
+                    Hai Hai Hai Hai Hai Hai Hai Hai Hai Hai 
+                    Hai Hai Hai Hai Hai Hai Hai Hai Hai Hai 
+                    Hai Hai Hai Hai Hai Hai Hai Hai Hai 
+                    Hai Hai Hai Hai Hai Hai Hai Hai Hai Hai Hai Hai 
+                    Hai Hai Hai Hai Hai Hai Hai Hai Hai Hai Hai 
+                    Article-tHREE
+            </p>
+            <p>
+                    Hai Hai Hai Hai Hai Hai Hai Hai Hai Hai 
+                    Hai Hai Hai Hai Hai Hai Hai Hai Hai Hai 
+                    Hai Hai Hai Hai Hai Hai Hai Hai Hai 
+                    Hai Hai Hai Hai Hai Hai Hai Hai Hai Hai Hai Hai 
+                    Hai Hai Hai Hai Hai Hai Hai Hai Hai Hai Hai 
+                    Article-tHREE
+            </p>`
+};
+
+function createTemplate(data)
+{
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+    var htmlTemplate = `
+                <!doctype html>
+                <html>
+                    <head>
+                            <title> ${title} </title>
+                            <meta name="viewport" content="width=device-width,initial-scale=1"/>
+                            <link href="/ui/style.css" rel="stylesheet" />
+                            
+                    </head>
+                    <body>
+                        <div class="container">
+                            <div>
+                                <a href="/"></a>
+                            </div>
+                            <hr/>
+                                <h3> ${heading} </h3>
+                            <div>
+                                <p>
+                                    ${date}
+                                </p>
+                            </div>
+                            ${content}
+                        </div>
+                    </body>
+                </html>
+    `;
+    return htmlTemplate;
+}
+
 app.get('/', function (req, res) { //handling specific url. get request is coming to / the below function will get executed
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -13,7 +71,8 @@ app.get('/', function (req, res) { //handling specific url. get request is comin
 
 app.get('/article-one',function(req,res)
 {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    //res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two',function(req,res)
