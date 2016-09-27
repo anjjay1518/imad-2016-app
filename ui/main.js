47285930b9a71cff99a3c7f6e4c03ee8541322e6
anjjay1518 <anjjay1518@gmail.com>
2016-09-27 12:59:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -46,4 +46,7 @@ button.onclick = function()
             }
         }
     };
+    
+    request.open('GET','http://http://anjjay1518.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
