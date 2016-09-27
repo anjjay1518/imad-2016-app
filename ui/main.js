de0daa8f7a5a553846506e64e6e800c9b1fda5ba
anjjay1518 <anjjay1518@gmail.com>
2016-09-27 15:04:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -36,14 +36,16 @@ button.onclick = function()
         {
             if(request.status === 200)
             {
-                var counter = request.responseText;
+               /* //incrementing the counter 
+               var counter = request.responseText;
                 var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
+                span.innerHTML = counter.toString();*/
                 
             }
         }
     };
-    request.open('GET','http://anjjay1518.imad.hasura-app.io/counter',true);
+    //request.open('GET','http://anjjay1518.imad.hasura-app.io/counter',true);
+    request.open('GET','http://anjjay1518.imad.hasura-app.io/submit-name?name' + name,true);
     request.send(null);
 };
 
@@ -52,7 +54,7 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_button');
 submit.onclick = function()
 {
-  var names = ['name1','name2','name3','name4','name5'];  
+  /*var names = ['name1','name2','name3','name4','name5'];  
   var list='';
   for(var i=0;i<names.length;i++)
   {
@@ -60,6 +62,37 @@ submit.onclick = function()
   }
   
   var ul = document.getElementById('namelist');
-  ul.innerHTML = list;
+  ul.innerHTML = list;*/
+  
+   var request = new XMLHttpRequest();
+    request.onreadystatechange = function()
+    {
+        if(request.readyState === XMLHttpRequest.DONE )
+        {
+            if(request.status === 200)
+            {
+               /* //incrementing the counter 
+               var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();*/
+                
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list='';
+                  for(var i=0;i<names.length;i++)
+                  {
+                      list += '<li>' + names[i] + '</li>';
+                  }
+                  
+                  var ul = document.getElementById('namelist');
+                  ul.innerHTML = list;
+                
+            }
+        }
+    };
+    //request.open('GET','http://anjjay1518.imad.hasura-app.io/counter',true);
+    request.open('GET','http://anjjay1518.imad.hasura-app.io/submit-name?name' + name,true);
+    request.send(null);
+  
 };
 
