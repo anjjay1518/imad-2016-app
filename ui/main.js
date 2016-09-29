10dde2f1513543b173a02a5d059d0ea5d103230f
anjjay1518 <anjjay1518@gmail.com>
2016-09-29 15:19:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,107 +1,186 @@
-/*console.log('Loaded!');
+var express = require('express');// importing software packages or libraries. express library is used to create web servers
+var morgan = require('morgan'); // morgan is used for the output logs of the server
+var path = require('path');
 
-var element = document.getElementById('id');
-element.innerHTML = "Anjjay";
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight()
-{
-    marginLeft = marginLeft + 2;
-    if(marginLeft >=400)
-    {
-        alert("Reaches the limit");
-    }
-    else
-    {
-        img.style.marginLeft = marginLeft + 'px';
-    }
-}
-img.onclick = function()
-{
-    var interval = setInterval(moveRight,100);
-    
-};*/
+var app = express();
+app.use(morgan('combined'));
 
-//For counter video
 
-var button = document.getElementById('counter');
-//var counter =0;
-button.onclick = function()
-{
-    //counter +=1;
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function()
-    {
-        if(request.readyState === XMLHttpRequest.DONE )
-        {
-            if(request.status === 200)
-            {
-               /* //incrementing the counter 
-               var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();*/
+var articles = {
+    'article-one' : {
+        title:'Anjjay karthick | Webapp',
+        date:'Sep 15 2016',
+        heading:'Article-One',
+        content:`<p>
+                        Hai Hai Hai Hai Hai Hai Hai Hai Hai Hai 
+                       
+                        <b>Article-ONE</b>
+                </p>
+                <hr/>
+                <div>
+                            Please Enter your valuable comments
+                            <input type="text" id="text" placeholder="feedback"/>
+                            <input type="button" value="submit" id="submit" />
+                </div>
+                <div class="footer">
+                            <ul id="fbul">
+                            
+                            </ul>
+                    </div>
+              `
+    },
+    'article-two' : {
+        title:'Anjjay karthick | Webapp',
+        date:'Sep 20 2016',
+        heading:'Article-Two',
+        content:`<p>
+                        Hai Hai Hai Hai Hai Hai Hai Hai Hai Hai 
+                        Hai Hai Hai Hai Hai Hai Hai Hai Hai Hai 
+                        <b>Article-TWO</b>
+                </p>
+                <hr/>
+                <div>
+                            Please Enter your valuable comments
+                            <input type="text" id="text" placeholder="feedback"/>
+                            <input type="button" value="submit" id="submit" />
+                </div>
+                <div class="footer">
+                            <ul id="fbul">
+                            
+                            </ul>
+                    </div>
+                `
                 
-            }
-        }
-    };
-    //request.open('GET','http://anjjay1518.imad.hasura-app.io/counter',true);
-    request.open('GET','http://anjjay1518.imad.hasura-app.io/submit-name?name' + name,true);
-    request.send(null);
+    },
+    'article-three' : {
+        title:'Anjjay karthick | Webapp',
+        date:'Sep 23 2016',
+        heading:'Article-Three',
+        content:`<p>
+                        Hai Hai Hai Hai Hai Hai Hai Hai Hai Hai 
+                        Hai Hai Hai Hai Hai Hai Hai Hai Hai Hai 
+                        <b>Article-THREE</b>
+                
+                </p>
+                <hr/>
+                <div>
+                            Please Enter your valuable comments
+                            <input type="text" id="text" placeholder="feedback"/>
+                            <input type="button" value="submit" id="submit" />
+                </div>
+                <div class="footer">
+                            <ul id="fbul">
+                            
+                            </ul>
+                    </div>
+                `
+    }
+    
 };
 
-
-var submit = document.getElementById('submit_button');
-submit.onclick = function()
+function createTemplate(data)
 {
-  /*var names = ['name1','name2','name3','name4','name5'];  
-  var list='';
-  for(var i=0;i<names.length;i++)
-  {
-      list += '<li>' + names[i] + '</li>';
-  }
-  
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = list;*/
-  
-   var request = new XMLHttpRequest();
-    request.onreadystatechange = function()
-    {
-        if(request.readyState === XMLHttpRequest.DONE )
-        {
-            if(request.status === 200)
-            {
-               /* //incrementing the counter 
-               var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();*/
-                names = ' ';
-                /*var names = request.responseText;
-                names = JSON.parse(names);
-                var list='';
-                  for(var i=0;i<names.length;i++)
-                  {
-                      list += '<li>' + names[i] + '</li>';
-                  }
-                  
-                  var ul = document.getElementById('namelist');
-                  ul.innerHTML = list;*/
-                  var feedback = document.getElementById("text");
-                  feedback = JSON.parse(feedback);
-                  var fblist = ' ';
-                  for(var i=0;i<feeback.length;i++)
-                  {
-                      fblist += '<li'> + feedback[i] + '</li>';
-                  }
-                    var ul = document.getElementById('fbul');
-                    ul.innerHTML = fblist;
-            }
-        }
-    };
-    //request.open('GET','http://anjjay1518.imad.hasura-app.io/counter',true);
-    var nameInput = document.getElementById('text');
-    var fbs = nameInput.value;
-    request.open('GET','http://anjjay1518.imad.hasura-app.io/submit-name?name=' + fbs,true);
-    request.send(null);
-  
-};
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+   
+    var htmlTemplate = `
+                <!doctype html>
+                <html>
+                    <head>
+                    
+                            <title> ${title} </title>
+                            <meta name="viewport" content="width=device-width,initial-scale=1"/>
+                            <link href="/ui/style.css" rel="stylesheet" />
+                            
+                    </head>
+                    <body>
+                        <div class="container">
+                            <div>
+                                <a href="/">Home</a>
+                            </div>
+                            <hr/>
+                                <h3> ${heading} </h3>
+                            <div>
+                                <p>
+                                    ${date}
+                                </p>
+                            </div>
+                            <p>
+                                ${content}
+                            </p>
+                        </div>
+                    
+                    </body>
+                </html>`
+                ;
+    return htmlTemplate; 
+}
 
+app.get('/', function (req, res) { //handling specific url. get request is coming to / the below function will get executed
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+/*var counter =0;
+app.get('/counter',function(req,res){
+   
+        counter +=1;
+        res.send(counter.toString());
+    
+});*/
+
+
+//Edited for the names
+var names = [];
+app.get('/submit-name',function(req,res)
+{
+	//Get the name from the rquest
+	//var name = req.params.name; //this is thro the paramater what we are entering
+	// but we need to take from the url like /submit-name?name=XXXX
+	var name = req.query.name;
+	names.push(name);
+	res.send(JSON.stringify(names));
+});
+
+//Edited for the comments
+/*var feedback = [];
+app.get('/feedback',function(req,res)
+{
+	//Get the name from the rquest
+	//var name = req.params.name; //this is thro the paramater what we are entering
+	// but we need to take from the url like /submit-name?name=XXXX
+	var fb = document.getElementById("text");
+	feedback.push(fb);
+	res.send(JSON.stringify(feedback));
+});*/
+
+//Edited for three articles
+
+app.get('/:articleName',function(req,res)
+{
+    //articleName = article-one
+    //res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    var articleName = req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
+});
+
+app.get('/ui/main.js', function (req, res) { // if this particular url is rquested this one the below function will executed as a response.
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/ui/style.css', function (req, res) { // if this particular url is rquested this one the below function will executed as a response.
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
+  console.log('IMAD course app listening on port ${port}!');
+});
+
+
+// server.js is the source code for the server file.it is actually executed.
