c33d3c96d43308c3da5e8a7c5e6cfd6f8f02b990
anjjay1518 <anjjay1518@gmail.com>
2016-09-29 13:06:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ function createTemplate(data)
     var heading=data.heading;
     var content=data.content;
     var feedback=data.feedback;
-    var htmlTemplate = 
+    var htmlTemplate = `
                 <!doctype html>
                 <html>
                     <head>
@@ -96,7 +96,7 @@ function createTemplate(data)
                         
                         ${feedback}
                     </body>
-                </html>
+                </html>`
                 ;
     return htmlTemplate; 
 }
